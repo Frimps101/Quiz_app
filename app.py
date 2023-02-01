ebcb0ecda6c94fef185d9a1f6b3d5948b8c2b547
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-02-01 22:18:58 +0000

    Adding enumerate too make the app more forgiving

--- app.py
+++ app.py
@@ -21,16 +21,48 @@ https://realpython.com/python-quiz-application/
 #     print(f"The answer is 'input', not {answer!r}")
 
 # To obey DRY, a list of tuples is used to hold questions and answers
-QUESTIONS =[
-    ("When was the first known use of the word 'quiz'", "1781"),
-    ("Which built-in function can get information from the user","input")
-]
+# QUESTIONS =[
+#     ("When was the first known use of the word 'quiz'", "1781"),
+#     ("Which built-in function can get information from the user","input")
+# ]
 
-for question, correct_answer in QUESTIONS:
-    answer = input(f"{question}?")
+# for question, correct_answer in QUESTIONS:
+#     answer = input(f"{question}?")
     
-    if answer == correct_answer:
-        print("Correct!")
-    else:
-        print(f"The answer is {correct_answer!r} not {answer!r}")
+#     if answer == correct_answer:
+#         print("Correct!")
+#     else:
+#         print(f"The answer is {correct_answer!r} not {answer!r}")
         
+
+# QUESTIONS = {
+#     "When was the first known use of the word 'quiz'": [
+#         "1781", "1771", "1871", "1881"
+#     ],
+#     "Which built-in function can get information from the user": [
+#         "input", "get", "print", "write"
+#     ],
+#     "Which keyword do you use to loop over a given list of elements": [
+#         "for", "while", "each", "loop"
+#     ],
+#     "What's the purpose of the built-in zip() function": [
+#         "To iterate over two or more sequences at the same time",
+#         "To combine several strings into one",
+#         "To compress several files into one archive",
+#         "To get information from the user",
+#     ],
+# }
+
+# for question, alternatives in QUESTIONS.items():
+#     correct_answer = alternatives[0]
+#     for alternative in sorted(alternatives):
+#         print(f"  - {alternative}")
+
+#     answer = input(f"{question}? ")
+#     if answer == correct_answer:
+#         print("Correct!")
+#     else:
+#         print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+# Adding the enumerate() function to make the app more forgiving
+# By adding a label to the answer
