73e508e6955c15e4e3228bba7ca6819bc61f7d51
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-02-02 21:15:35 +0000

    Making the app more user-friendly

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ A simple python quiz app
 https://realpython.com/python-quiz-application/
 """
 
+from string import ascii_lowercase
+
 # !r indicates that answer should be inserted based on its repr() representation. In practice, this means that strings are shown surrounded by single quotes, like '1871'
 
 
@@ -70,10 +72,37 @@ https://realpython.com/python-quiz-application/
 
 # Update the application to use enumerate() to print the index of each answer alternative
 
+# QUESTIONS = {
+#     "Which keyword do you use to loop over a given list of elements": [
+#         "for", "while", "each", "loop"
+#     ],
+#     "What's the purpose of the built-in zip() function": [
+#         "To iterate over two or more sequences at the same time",
+#         "To combine several strings into one",
+#         "To compress several files into one archive",
+#         "To get information from the user",
+#     ],
+#     "What's the name of Python's sorting algorithm": [
+#         "Timsort", "Quicksort", "Merge sort", "Bubble sort"
+#     ],
+# }
+
+# for question, alternatives in QUESTIONS.items():
+#     correct_answer = alternatives[0]
+#     sorted_alternatives = sorted(alternatives)
+#     for label, alternative in enumerate(sorted_alternatives):
+#         print(f" {label}) {alternative}")
+        
+#     answer_label = int(input(f"{question}?"))
+#     answer = sorted_alternatives[answer_label]
+#     if answer == correct_answer:
+#         print("Correct")
+#     else:
+#         print(f"The answer is {correct_answer!r}, not {answer!r}")
+        
+# Format the ouput more nicely
+
 QUESTIONS = {
-    "Which keyword do you use to loop over a given list of elements": [
-        "for", "while", "each", "loop"
-    ],
     "What's the purpose of the built-in zip() function": [
         "To iterate over two or more sequences at the same time",
         "To combine several strings into one",
@@ -83,17 +112,22 @@ QUESTIONS = {
     "What's the name of Python's sorting algorithm": [
         "Timsort", "Quicksort", "Merge sort", "Bubble sort"
     ],
+    "What does dict.get(key) return if key isn't found in dict": [
+        "None", "key", "True", "False",
+    ]
 }
 
-for question, alternatives in QUESTIONS.items():
+for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
     correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
-    for label, alternative in enumerate(sorted_alternatives):
-        print(f" {label}) {alternative}")
-        
-    answer_label = int(input(f"{question}?"))
-    answer = sorted_alternatives[answer_label]
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    answer_label = input("\nChoice? ")
+    answer = labeled_alternatives.get(answer_label)
     if answer == correct_answer:
-        print("Correct")
+        print("⭐ Correct! ⭐")
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
